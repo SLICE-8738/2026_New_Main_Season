e5e8fa9da961a0a6b621f6791fcd8c596c56bdca
SLICE <fvhsrobotics@gmail.com>
2026-03-20 13:18:16 -0400

    autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,11 +8,13 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
+import java.util.HashMap;
 import java.util.concurrent.locks.AbstractQueuedLongSynchronizer.ConditionObject;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -133,7 +135,17 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
+        // HashMap<String, Command> eventMap = new HashMap<>();
+        // eventMap.put("67676767676776767", m_ExtendIntake);
 
+
+        NamedCommands.registerCommand("Spin Intake", m_Spintake);
+        NamedCommands.registerCommand("Stop Intake", m_Stoptake);
+        NamedCommands.registerCommand("Extend Intake", m_ExtendIntake);
+      //  NamedCommands.registerCommand("Retract Intake", m_RetractIntake);
+        NamedCommands.registerCommand("67676767767", m_RetractIntake);
+
+        NamedCommands.registerCommand("Align and Shoot", m_alignAndShootHub);
         // ================
         // Driver Controls
         // ================
